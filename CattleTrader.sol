18cb9bd3be2a5107d2e7846362827297bdf6c357
Prabhdeep Singh <73354129+rj13-to@users.noreply.github.com>
2023-04-14 00:49:15 +0530

    Added basic structures of the program

--- CattleTrader.sol
+++ CattleTrader.sol
@@ -2,6 +2,42 @@
 
 
 pragma solidity ^0.8.4;
-contract CowTracker{
+contract CattleTrader{
+     uint userID = 0;
+    uint cowID = 0;
+    uint medID = 0;
+    uint dairyID = 0;
 
+    // medical history
+    struct Medical{
+        string problem;
+        string report;
+    }
+
+    // dairy
+    struct Dairy{
+        uint fat;
+        uint maxMilk;
+    }
+
+    // defination of cows
+    struct Cow{
+        uint owner; 
+        string breed;
+        uint dateOfBirth;
+        uint price;
+        uint[] tests;
+        uint[] dtests;
+    }
+
+    // owner of the cows
+    struct User{
+        string name;
+        uint[] cows;
+        string add;
+        uint dob;
+        uint minAge;
+        uint maxAge;
+        uint price;
+    }
 }
